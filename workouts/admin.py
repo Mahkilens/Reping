f4a9837176a4719d733e89f6a1a94381e952ccf5
Marquis Prescott <marquisprescott16@gmail.com>
2026-02-13 18:02:17 -0500

    chore: register workout models in Django admin

--- workouts/admin.py
+++ workouts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Exercise, WorkoutSession, SetEntry
 
 # Register your models here.
+admin.site.register(Exercise)
+admin.site.register(WorkoutSession)
+admin.site.register(SetEntry)
